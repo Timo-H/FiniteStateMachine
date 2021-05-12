0a94cdb082d28eda52f376827cfe9bda675bcd89
Timo Hak <timo.hak@casema.nl>
2021-05-12 18:45:32 +0200

    start unit testing

---
+++ src/Testing/Machine/FSMTest.java
@@ -0,0 +1,13 @@
+package Machine;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FSMTest {
+
+    @Test
+    void goThroughText() {
+        Character[] overgangsTekens = {'A', 'B'};
+    }
+}
---
+++ src/Testing/Machine/NodeTest.java
@@ -0,0 +1,16 @@
+package Machine;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @Test
+    void getOvergangen() {
+    }
+
+    @Test
+    void getNextNode() {
+    }
+}
